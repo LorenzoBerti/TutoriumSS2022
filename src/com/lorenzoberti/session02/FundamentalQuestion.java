891ecf7c00fcc3a3eb800a9665452f96e76a02f5
Lorenzo Berti <Lorenzo.Berti@campus.lmu.de>
2022-05-17 13:18:24 +0200

    clean

--- src/com/lorenzoberti/session02/FundamentalQuestion.java
+++ src/com/lorenzoberti/session02/FundamentalQuestion.java
@@ -26,7 +26,7 @@ public class FundamentalQuestion {
 			boolean word;
 			
 			do {
-				word = answer.equals("42");
+				word = answer.equals("42"); // "transform" the input in a boolean
 				
 				if(word) {
 					System.out.println("Great! You are a man of culture ;-)");
@@ -42,7 +42,7 @@ public class FundamentalQuestion {
 			
 				while (word == false);
 
-			in.close();
+				in.close();
 				
 		}
 
