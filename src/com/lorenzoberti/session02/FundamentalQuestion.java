284e98b970f1a5d6f2a55eaa5027d39b1e6ccca0
Lorenzo Berti <Lorenzo.Berti@campus.lmu.de>
2022-05-17 10:03:22 +0200

    fundamental question

---
+++ src/com/lorenzoberti/session02/FundamentalQuestion.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.lorenzoberti.session02;
+
+import java.util.Scanner;
+
+/**
+ * @author Lorenzo Berti
+ *
+ */
+public class FundamentalQuestion {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+		System.out.print("Which is the answer to the fundamental question?");
+		System.out.println();
+		Scanner in = new Scanner(System.in);
+			
+			String answer = in.nextLine();
+			
+			boolean word;
+			
+			do {
+				word = answer.equals("42");
+				
+				if(word) {
+					System.out.println("Great! You are a man of culture ;-)");
+				} else {
+
+					System.out.println("No, I'm sorry. Try again");
+					System.out.println();
+					System.out.print("Which is the answer to the fundamental question?");
+					answer = in.nextLine();
+
+				}
+			}
+			
+				while (word == false);
+
+			in.close();
+				
+		}
+
+
+
+
+}
