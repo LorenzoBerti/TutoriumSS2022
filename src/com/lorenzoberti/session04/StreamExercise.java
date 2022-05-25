354153ba5524afcbbe793d44f4d15ad4b1e9d7d9
Lorenzo Berti <Lorenzo.Berti@campus.lmu.de>
2022-05-25 16:33:15 +0200

    exercise session04

--- src/com/lorenzoberti/session04/StreamExercise.java
+++ src/com/lorenzoberti/session04/StreamExercise.java
@@ -4,9 +4,9 @@
 package com.lorenzoberti.session04;
 
 /**
- * Here you have to compute the value of Pi with Monte Carlo method by using
- * stream and the value of a digital option by using stream (handout 2 exercise
- * 2). Hint: for the option price you can use
+ * Here you have to compute the value of Pi with Monte Carlo method and the
+ * value of a digital option (handout 2 exercise 2) by using stream. Hint: for
+ * the option price you can use
  * com.andreamazzon.exercises.simulators.BinomialModelSimulator
  * 
  * @author Lorenzo Berti
@@ -19,6 +19,30 @@ public class StreamExercise {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+
+	}
+
+
+	/**
+	 * This method return the approximation of Pi by using Monte Carlo method.
+	 * 
+	 * @param numberOfDrawings
+	 * @return approximation of Pi
+	 */
+	private static double piMonteCarlo(int numberOfDrawings) {
+
+		double numberOfPointsInside = 0;
+
+		for (int i = 0; i < numberOfDrawings; i++) {
+			double x = 2 * (Math.random() - 0.5);
+			double y = 2 * (Math.random() - 0.5);
+			if (x * x + y * y < 1) {
+				numberOfPointsInside += 1;
+			}
+		}
+
+		return 4 * numberOfPointsInside / numberOfDrawings;
 
 	}
 
