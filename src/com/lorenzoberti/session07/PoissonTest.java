0feada9a34c739367fc385e7481c181d6d6f2d8d
Lorenzo Berti <Lorenzo.Berti@campus.lmu.de>
2022-06-29 10:32:14 +0200

    session08

--- src/com/lorenzoberti/session07/PoissonTest.java
+++ src/com/lorenzoberti/session07/PoissonTest.java
@@ -5,15 +5,16 @@ package com.lorenzoberti.session07;
 
 import java.text.DecimalFormat;
 import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+import java.util.function.DoubleUnaryOperator;
 
+import net.finmath.plots.Named;
+import net.finmath.plots.Plot2D;
 import net.finmath.randomnumbers.MersenneTwister;
 import net.finmath.randomnumbers.RandomNumberGenerator1D;
-import net.finmath.randomnumbers.SobolSequence1D;
 import net.finmath.randomnumbers.VanDerCorputSequence;
 
 /**
- * Use this class to test your implementation of the Poisson random variable.
- * 
  * @author Lorenzo Berti
  *
  */
@@ -33,27 +34,24 @@ public class PoissonTest {
 
 		double lambda = 0.5;
 
-		RandomVariable poisson = new Poisson(lambda);
+		AbstractRandomVariable poisson = new Poisson(lambda);
 		double mean = poisson.getAnalyticMean();
 		double variance = poisson.getAnalyticVariance();
 
 		RandomNumberGenerator1D mersenne = new MersenneTwister();
 		RandomNumberGenerator1D vanDerCorput2 = new VanDerCorputSequence(2);
 		RandomNumberGenerator1D vanDerCorput3 = new VanDerCorputSequence(3);
-		RandomNumberGenerator1D sobolev = new SobolSequence1D();
 
 		double sampleMeanRandom = poisson.getSampleMean(numberOfSimulations);
 		double sampleMeanMersenne = poisson.getSampleMean(numberOfSimulations, mersenne);
 		double sampleMeanCorput2 = poisson.getSampleMean(numberOfSimulations, vanDerCorput2);
 		double sampleMeanCorput3 = poisson.getSampleMean(numberOfSimulations, vanDerCorput3);
-		double sampleMeanSobolev = poisson.getSampleMean(numberOfSimulations, sobolev);
 		double sampleMeanEquidistributed = poisson.getSampleMeanEquidistributed(numberOfSimulations);
 
 		double sampleVarianceRandom = poisson.getSampleVariance(numberOfSimulations);
 		double sampleVarianceMersenne = poisson.getSampleVariance(numberOfSimulations, mersenne);
 		double sampleVarianceCorput2 = poisson.getSampleVariance(numberOfSimulations, vanDerCorput2);
 		double sampleVarianceCorput3 = poisson.getSampleVariance(numberOfSimulations, vanDerCorput3);
-		double sampleVarianceSobolev = poisson.getSampleVariance(numberOfSimulations, sobolev);
 		double sampleVarianceEquidistributed = poisson.getSampleVarianceEquidistributed(numberOfSimulations);
 
 		ChebyshevInterval interval = new ChebyshevInterval(poisson);
@@ -68,8 +66,6 @@ public class PoissonTest {
 				+ formatterValue.format(Math.abs(sampleMeanCorput2 - mean)));
 		System.out.println("Van Der Corput3..: " + formatterValue.format(sampleMeanCorput3) + "\tError: "
 				+ formatterValue.format(Math.abs(sampleMeanCorput3 - mean)));
-		System.out.println("Sobolev..........: " + formatterValue.format(sampleMeanSobolev) + "\tError: "
-				+ formatterValue.format(Math.abs(sampleMeanSobolev - mean)));
 		System.out.println("Equidistributed..: " + formatterValue.format(sampleMeanEquidistributed) + "\tError: "
 				+ formatterValue.format(Math.abs(sampleMeanEquidistributed - mean)));
 		System.out.println("Confidence Interval: " + Arrays.toString(confidenceInterval));
@@ -85,8 +81,6 @@ public class PoissonTest {
 				+ formatterValue.format(Math.abs(sampleVarianceCorput2 - variance)));
 		System.out.println("Van Der Corput3..: " + formatterValue.format(sampleVarianceCorput3) + "\tError: "
 				+ formatterValue.format(Math.abs(sampleVarianceCorput3 - variance)));
-		System.out.println("Sobolev..........: " + formatterValue.format(sampleVarianceSobolev) + "\tError: "
-				+ formatterValue.format(Math.abs(sampleVarianceSobolev - variance)));
 		System.out.println("Equidistributed..: " + formatterValue.format(sampleVarianceEquidistributed) + "\tError: "
 				+ formatterValue.format(Math.abs(sampleVarianceEquidistributed - variance)));
 
@@ -97,6 +91,7 @@ public class PoissonTest {
 		double density2 = poisson.getDensityFunction(2);
 		double density3 = poisson.getDensityFunction(3);
 		double cdf3 = poisson.getCumulativeDistributionFunction(3);
+
 		System.out
 				.println("Density in 1..: " + density1 + "\tError: " + Math.abs(density1 - Math.exp(-lambda) * lambda));
 		System.out.println("Density in 2..: " + density2 + "\tError: "
@@ -106,8 +101,89 @@ public class PoissonTest {
 		System.out.println(
 				"CDF in 3......: " + cdf3 + "\tError: " + Math.abs(cdf3 - density1 - density2 - density0 - density3));
 
+		// Plot the density
+		DecimalFormat formatterValuePlot = new DecimalFormat("#0.0000");
+		int evaluationPoints = 20;
+
+		Plot2D plotDensity = new Plot2D(0, evaluationPoints, evaluationPoints + 1,
+				Arrays.asList(new Named<DoubleUnaryOperator>("lambda = 1", densityLambda(1)),
+						new Named<DoubleUnaryOperator>("lambda = 2", densityLambda(2)),
+						new Named<DoubleUnaryOperator>("lambda = 3", densityLambda(3)),
+						new Named<DoubleUnaryOperator>("lambda = 10", densityLambda(10))));
+		plotDensity.setTitle("Probability density function");
+		plotDensity.setXAxisLabel("k");
+		plotDensity.setYAxisLabel("Density");
+		plotDensity.setYAxisNumberFormat(formatterValuePlot);
+		plotDensity.show();
+
+		// Plot the error mean
+
+		int numberOfTimeSteps = 10000;
+
+		Plot2D plotMathRandom = new Plot2D(0, numberOfTimeSteps, numberOfTimeSteps + 1, plotError(poisson));
+		plotMathRandom.setTitle("Error Math Random");
+		plotMathRandom.setXAxisLabel("NumberOfSimulations");
+		plotMathRandom.setYAxisLabel("Error");
+		plotMathRandom.setYAxisNumberFormat(formatterValuePlot);
+		plotMathRandom.show();
+
+		Plot2D plotMersenne = new Plot2D(0, numberOfTimeSteps, numberOfTimeSteps + 1, plotError(poisson, mersenne));
+		plotMersenne.setTitle("Error Mersenne");
+		plotMersenne.setXAxisLabel("NumberOfSimulations");
+		plotMersenne.setYAxisLabel("Error");
+		plotMersenne.setYAxisNumberFormat(formatterValuePlot);
+		plotMersenne.show();
+
+		Plot2D plotVanDerCorput2 = new Plot2D(0, numberOfTimeSteps, numberOfTimeSteps + 1,
+				plotError(poisson, vanDerCorput2));
+		plotVanDerCorput2.setTitle("Error Van Der Corput 2");
+		plotVanDerCorput2.setXAxisLabel("NumberOfSimulations");
+		plotVanDerCorput2.setYAxisLabel("Error");
+		plotVanDerCorput2.setYAxisNumberFormat(formatterValuePlot);
+		plotVanDerCorput2.show();
+
+		Plot2D plotVanDerCorput3 = new Plot2D(0, numberOfTimeSteps, numberOfTimeSteps + 1,
+				plotError(poisson, vanDerCorput3));
+		plotVanDerCorput3.setTitle("Error Van Der Corput 3");
+		plotVanDerCorput3.setXAxisLabel("NumberOfSimulations");
+		plotVanDerCorput3.setYAxisLabel("Error");
+		plotVanDerCorput3.setYAxisNumberFormat(formatterValuePlot);
+		plotVanDerCorput3.show();
 
 	}
 
-}
+	static DoubleUnaryOperator plotError(AbstractRandomVariable randomVariable, RandomNumberGenerator1D random) {
 
+		double mean = randomVariable.getAnalyticMean();
+		DoubleUnaryOperator errorFunction = n -> {
+			try {
+				return Math.abs(randomVariable.getSampleMeanParallel((int) n, random) - mean);
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}
+			return 0;
+		};
+		return errorFunction;
+	}
+
+	static DoubleUnaryOperator plotError(AbstractRandomVariable randomVariable) {
+
+		double mean = randomVariable.getAnalyticMean();
+		DoubleUnaryOperator errorFunction = n -> {
+			try {
+				return Math.abs(randomVariable.getSampleMeanParallel((int) n) - mean);
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}
+			return 0;
+		};
+		return errorFunction;
+	}
+
+	static DoubleUnaryOperator densityLambda(double lambda) {
+
+		AbstractRandomVariable poissonLambda = new Poisson(lambda);
+		DoubleUnaryOperator density = (n) -> poissonLambda.getDensityFunction((int) n);
+		return density;
+	}
+}
--- src/com/lorenzoberti/session08/ParetoTest.java
+++ src/com/lorenzoberti/session08/ParetoTest.java
@@ -6,10 +6,14 @@ package com.lorenzoberti.session08;
 import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
+import java.util.function.DoubleUnaryOperator;
 
 import com.lorenzoberti.session07.AbstractRandomVariable;
 import com.lorenzoberti.session07.ChebyshevInterval;
+import com.lorenzoberti.session07.ConfidenceInterval;
 
+import net.finmath.plots.Named;
+import net.finmath.plots.Plot2D;
 import net.finmath.randomnumbers.MersenneTwister;
 import net.finmath.randomnumbers.RandomNumberGenerator1D;
 import net.finmath.randomnumbers.SobolSequence1D;
@@ -35,7 +39,7 @@ public class ParetoTest {
 	 */
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-		double shape = 1;
+		double shape = 3;
 		double scale = 1;
 
 		AbstractRandomVariable pareto = new Pareto(shape, scale);
@@ -97,130 +101,130 @@ public class ParetoTest {
 
 		System.out.println("---------------------------------------");
 
-//		AbstractRandomVariable pareto1 = new Pareto(1, scale);
-//		AbstractRandomVariable pareto2 = new Pareto(2, scale);
-//		AbstractRandomVariable pareto3 = new Pareto(3, scale);
-//		AbstractRandomVariable pareto10 = new Pareto(10, scale);
-//		DoubleUnaryOperator cdf1 = (x) -> pareto1.getCumulativeDistributionFunction(x);
-//		DoubleUnaryOperator cdf2 = (x) -> pareto2.getCumulativeDistributionFunction(x);
-//		DoubleUnaryOperator cdf3 = (x) -> pareto3.getCumulativeDistributionFunction(x);
-//		DoubleUnaryOperator cdf10 = (x) -> pareto10.getCumulativeDistributionFunction(x);
+		AbstractRandomVariable pareto1 = new Pareto(1, scale);
+		AbstractRandomVariable pareto2 = new Pareto(2, scale);
+		AbstractRandomVariable pareto3 = new Pareto(3, scale);
+		AbstractRandomVariable pareto10 = new Pareto(10, scale);
+		DoubleUnaryOperator cdf1 = (x) -> pareto1.getCumulativeDistributionFunction(x);
+		DoubleUnaryOperator cdf2 = (x) -> pareto2.getCumulativeDistributionFunction(x);
+		DoubleUnaryOperator cdf3 = (x) -> pareto3.getCumulativeDistributionFunction(x);
+		DoubleUnaryOperator cdf10 = (x) -> pareto10.getCumulativeDistributionFunction(x);
 
-//		int numberOfTimeSteps = 100;
-//		Plot2D plot = new Plot2D(0, 10, numberOfTimeSteps,
-//				Arrays.asList(new Named<DoubleUnaryOperator>("Shape = 1", cdf1),
-//						new Named<DoubleUnaryOperator>("Shape = 2", cdf2),
-//						new Named<DoubleUnaryOperator>("Shape = 3", cdf3),
-//						new Named<DoubleUnaryOperator>("Shape = 10", cdf10)));
-//		plot.setTitle("Pareto distribution cdf");
-//		plot.setXAxisLabel("x");
-//		plot.setYAxisLabel("F(x)");
-//		plot.setIsLegendVisible(true);
-//		plot.show();
-//
-//		DoubleUnaryOperator density1 = (x) -> pareto.getDensityFunction(x);
-//		DoubleUnaryOperator density2 = (x) -> pareto2.getDensityFunction(x);
-//		DoubleUnaryOperator density3 = (x) -> pareto3.getDensityFunction(x);
-//		DoubleUnaryOperator density10 = (x) -> pareto10.getDensityFunction(x);
-//
-//		Plot2D plot2 = new Plot2D(0.9, 2, numberOfTimeSteps,
-//				Arrays.asList(new Named<DoubleUnaryOperator>("Shape = 1", density1),
-//						new Named<DoubleUnaryOperator>("Shape = 2", density2),
-//						new Named<DoubleUnaryOperator>("Shape = 3", density3),
-//						new Named<DoubleUnaryOperator>("Shape = 10", density10)));
-//		plot2.setTitle("Pareto density");
-//		plot2.setXAxisLabel("x");
-//		plot2.setYAxisLabel("f(x)");
-//		plot2.setIsLegendVisible(true);
-//		plot2.show();
+		int numberOfTimeSteps = 100;
+		Plot2D plot = new Plot2D(0, 10, numberOfTimeSteps,
+				Arrays.asList(new Named<DoubleUnaryOperator>("Shape = 1", cdf1),
+						new Named<DoubleUnaryOperator>("Shape = 2", cdf2),
+						new Named<DoubleUnaryOperator>("Shape = 3", cdf3),
+						new Named<DoubleUnaryOperator>("Shape = 10", cdf10)));
+		plot.setTitle("Pareto distribution cdf");
+		plot.setXAxisLabel("x");
+		plot.setYAxisLabel("F(x)");
+		plot.setIsLegendVisible(true);
+		plot.show();
 
-//		System.out.println("_".repeat(80) + "\n");
-//
-//		System.out.println("Confidence Interval Chebyshev: " + Arrays.toString(confidenceInterval));
-//
-//		int numberOfMeanComputations = 100;
-//
-//		System.out.println("_".repeat(80) + "\n");
-//
-//		System.out.println("Math random: ");
-//		System.out.println("The frequence of mean being in the Chebyshev confidence interval is "
-//				+ formatterPercentage.format(frequenceOfInterval(pareto, interval, numberOfMeanComputations, level)));
-//
-//		System.out.println("_".repeat(80) + "\n");
-//
-//		System.out.println("Mersenne: ");
-//		System.out.println("The frequence of p being in the Chebyshev confidence interval is " + formatterPercentage
-//				.format(frequenceOfInterval(pareto, mersenne, interval, numberOfMeanComputations, level)));
-//
-//		System.out.println("_".repeat(80) + "\n");
-//
-//		System.out.println("Van Der Corput 2: ");
-//		System.out.println("The frequence of p being in the Chebyshev confidence interval is " + formatterPercentage
-//				.format(frequenceOfInterval(pareto, vanDerCorput2, interval, numberOfMeanComputations, level)));
-//
-//		System.out.println("_".repeat(80) + "\n");
-//
-//		System.out.println("Van Der Corput 3: ");
-//		System.out.println("The frequence of p being in the Chebyshev confidence interval is " + formatterPercentage
-//				.format(frequenceOfInterval(pareto, vanDerCorput3, interval, numberOfMeanComputations, level)));
-//
-//		System.out.println("_".repeat(80) + "\n");
-//
-//		System.out.println("Equidistributed: ");
-//		System.out.println("The frequence of p being in the Chebyshev confidence interval is " + formatterPercentage
-//				.format(frequenceOfIntervalEquidistributed(pareto, interval, numberOfMeanComputations, level)));
-//	}
-//
-//	public static double frequenceOfInterval(AbstractRandomVariable randomVariable, ConfidenceInterval interval,
-//			int numberOfMeanComputations, double confidenceLevel) throws InterruptedException, ExecutionException {
-//
-//		double numberOfTimesInsideTheInterval = 0;
-//		double[] confidenceInterval = interval.getConfidenceInterval(numberOfSimulations, confidenceLevel);
-//		double lowerBound = confidenceInterval[0];
-//		double upperBound = confidenceInterval[1];
-//		double sampleMean;
-//		for (int i = 0; i < numberOfMeanComputations; i++) {
-//			sampleMean = randomVariable.getSampleMeanParallel(numberOfSimulations); // sample mean
-//			if (sampleMean > lowerBound && sampleMean < upperBound) {
-//				numberOfTimesInsideTheInterval++; // sample mean within the confidence interval
-//			}
-//		}
-//		return numberOfTimesInsideTheInterval / numberOfMeanComputations;
-//	}
-//
-//	public static double frequenceOfInterval(AbstractRandomVariable randomVariable, RandomNumberGenerator1D random,
-//			ConfidenceInterval interval, int numberOfMeanComputations, double confidenceLevel)
-//			throws InterruptedException, ExecutionException {
-//
-//		double numberOfTimesInsideTheInterval = 0;
-//		double[] confidenceInterval = interval.getConfidenceInterval(numberOfSimulations, confidenceLevel);
-//		double lowerBound = confidenceInterval[0];
-//		double upperBound = confidenceInterval[1];
-//		double sampleMean;
-//		for (int i = 0; i < numberOfMeanComputations; i++) {
-//			sampleMean = randomVariable.getSampleMeanParallel(numberOfSimulations, random); // sample mean
-//			if (sampleMean > lowerBound && sampleMean < upperBound) {
-//				numberOfTimesInsideTheInterval++; // sample mean within the confidence interval
-//			}
-//		}
-//		return numberOfTimesInsideTheInterval / numberOfMeanComputations;
-//	}
-//
-//	public static double frequenceOfIntervalEquidistributed(AbstractRandomVariable randomVariable,
-//			ConfidenceInterval interval, int numberOfMeanComputations, double confidenceLevel) {
-//
-//		double numberOfTimesInsideTheInterval = 0;
-//		double[] confidenceInterval = interval.getConfidenceInterval(numberOfSimulations, confidenceLevel);
-//		double lowerBound = confidenceInterval[0];
-//		double upperBound = confidenceInterval[1];
-//		double sampleMean;
-//		for (int i = 0; i < numberOfMeanComputations; i++) {
-//			sampleMean = randomVariable.getSampleMeanEquidistributed(numberOfSimulations); // sample mean
-//			if (sampleMean > lowerBound && sampleMean < upperBound) {
-//				numberOfTimesInsideTheInterval++; // sample mean within the confidence interval
-//			}
-//		}
-//		return numberOfTimesInsideTheInterval / numberOfMeanComputations;
+		DoubleUnaryOperator density1 = (x) -> pareto.getDensityFunction(x);
+		DoubleUnaryOperator density2 = (x) -> pareto2.getDensityFunction(x);
+		DoubleUnaryOperator density3 = (x) -> pareto3.getDensityFunction(x);
+		DoubleUnaryOperator density10 = (x) -> pareto10.getDensityFunction(x);
+
+		Plot2D plot2 = new Plot2D(0.9, 2, numberOfTimeSteps,
+				Arrays.asList(new Named<DoubleUnaryOperator>("Shape = 1", density1),
+						new Named<DoubleUnaryOperator>("Shape = 2", density2),
+						new Named<DoubleUnaryOperator>("Shape = 3", density3),
+						new Named<DoubleUnaryOperator>("Shape = 10", density10)));
+		plot2.setTitle("Pareto density");
+		plot2.setXAxisLabel("x");
+		plot2.setYAxisLabel("f(x)");
+		plot2.setIsLegendVisible(true);
+		plot2.show();
+
+		System.out.println("_".repeat(80) + "\n");
+
+		System.out.println("Confidence Interval Chebyshev: " + Arrays.toString(confidenceInterval));
+
+		int numberOfMeanComputations = 100;
+
+		System.out.println("_".repeat(80) + "\n");
+
+		System.out.println("Math random: ");
+		System.out.println("The frequence of mean being in the Chebyshev confidence interval is "
+				+ formatterPercentage.format(frequenceOfInterval(pareto, interval, numberOfMeanComputations, level)));
+
+		System.out.println("_".repeat(80) + "\n");
+
+		System.out.println("Mersenne: ");
+		System.out.println("The frequence of p being in the Chebyshev confidence interval is " + formatterPercentage
+				.format(frequenceOfInterval(pareto, mersenne, interval, numberOfMeanComputations, level)));
+
+		System.out.println("_".repeat(80) + "\n");
+
+		System.out.println("Van Der Corput 2: ");
+		System.out.println("The frequence of p being in the Chebyshev confidence interval is " + formatterPercentage
+				.format(frequenceOfInterval(pareto, vanDerCorput2, interval, numberOfMeanComputations, level)));
+
+		System.out.println("_".repeat(80) + "\n");
+
+		System.out.println("Van Der Corput 3: ");
+		System.out.println("The frequence of p being in the Chebyshev confidence interval is " + formatterPercentage
+				.format(frequenceOfInterval(pareto, vanDerCorput3, interval, numberOfMeanComputations, level)));
+
+		System.out.println("_".repeat(80) + "\n");
+
+		System.out.println("Equidistributed: ");
+		System.out.println("The frequence of p being in the Chebyshev confidence interval is " + formatterPercentage
+				.format(frequenceOfIntervalEquidistributed(pareto, interval, numberOfMeanComputations, level)));
+	}
+
+	public static double frequenceOfInterval(AbstractRandomVariable randomVariable, ConfidenceInterval interval,
+			int numberOfMeanComputations, double confidenceLevel) throws InterruptedException, ExecutionException {
+
+		double numberOfTimesInsideTheInterval = 0;
+		double[] confidenceInterval = interval.getConfidenceInterval(numberOfSimulations, confidenceLevel);
+		double lowerBound = confidenceInterval[0];
+		double upperBound = confidenceInterval[1];
+		double sampleMean;
+		for (int i = 0; i < numberOfMeanComputations; i++) {
+			sampleMean = randomVariable.getSampleMeanParallel(numberOfSimulations); // sample mean
+			if (sampleMean > lowerBound && sampleMean < upperBound) {
+				numberOfTimesInsideTheInterval++; // sample mean within the confidence interval
+			}
+		}
+		return numberOfTimesInsideTheInterval / numberOfMeanComputations;
+	}
+
+	public static double frequenceOfInterval(AbstractRandomVariable randomVariable, RandomNumberGenerator1D random,
+			ConfidenceInterval interval, int numberOfMeanComputations, double confidenceLevel)
+			throws InterruptedException, ExecutionException {
+
+		double numberOfTimesInsideTheInterval = 0;
+		double[] confidenceInterval = interval.getConfidenceInterval(numberOfSimulations, confidenceLevel);
+		double lowerBound = confidenceInterval[0];
+		double upperBound = confidenceInterval[1];
+		double sampleMean;
+		for (int i = 0; i < numberOfMeanComputations; i++) {
+			sampleMean = randomVariable.getSampleMeanParallel(numberOfSimulations, random); // sample mean
+			if (sampleMean > lowerBound && sampleMean < upperBound) {
+				numberOfTimesInsideTheInterval++; // sample mean within the confidence interval
+			}
+		}
+		return numberOfTimesInsideTheInterval / numberOfMeanComputations;
+	}
+
+	public static double frequenceOfIntervalEquidistributed(AbstractRandomVariable randomVariable,
+			ConfidenceInterval interval, int numberOfMeanComputations, double confidenceLevel) {
+
+		double numberOfTimesInsideTheInterval = 0;
+		double[] confidenceInterval = interval.getConfidenceInterval(numberOfSimulations, confidenceLevel);
+		double lowerBound = confidenceInterval[0];
+		double upperBound = confidenceInterval[1];
+		double sampleMean;
+		for (int i = 0; i < numberOfMeanComputations; i++) {
+			sampleMean = randomVariable.getSampleMeanEquidistributed(numberOfSimulations); // sample mean
+			if (sampleMean > lowerBound && sampleMean < upperBound) {
+				numberOfTimesInsideTheInterval++; // sample mean within the confidence interval
+			}
+		}
+		return numberOfTimesInsideTheInterval / numberOfMeanComputations;
 	}
 
 }
