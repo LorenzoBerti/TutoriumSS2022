12d45c8f5e3c78515c9344af3729ee8bed9265e8
Lorenzo Berti <Lorenzo.Berti@campus.lmu.de>
2022-07-05 14:11:53 +0200

    java doc

--- src/com/lorenzoberti/session09/LevyProcess.java
+++ src/com/lorenzoberti/session09/LevyProcess.java
@@ -14,18 +14,63 @@ import net.finmath.stochastic.RandomVariable;
  */
 public interface LevyProcess {
 
+	/**
+	 * This method returns the all paths of the Lévy process.
+	 * 
+	 * @return the paths of the Lévy process.
+	 */
 	RandomVariable[] getPaths();
 
+	/**
+	 * This method returns the Lévy process at a given time index.
+	 * 
+	 * @param timeIndex
+	 * @return the process at the given time index.
+	 */
 	RandomVariable getProcessAtGivenTimeIndex(int timeIndex);
 
+	/**
+	 * This method returns the Lévy process at a given time.
+	 * 
+	 * @param time
+	 * @return the process at the given time.
+	 */
 	RandomVariable getProcessAtGivenTime(double time);
 
+	/**
+	 * This method returns the specific path of the Lévy process as an array of
+	 * doubles.
+	 * 
+	 * @param pathIndex
+	 * @return the specific path of the Lévy process.
+	 */
 	double[] getSpecificPath(int pathIndex);
 
+	/**
+	 * This method returns the realization of the specific path of the Lévy process
+	 * at the given time index.
+	 * 
+	 * @param pathIndex
+	 * @param timeIndex
+	 * @return the specific realization of the Lévy process.
+	 */
 	double getSpecificRealizationAtGivenTimeIndex(int pathIndex, int timeIndex);
 
+	/**
+	 * This method returns the realization of the specific path of the Lévy process
+	 * at the given time.
+	 * 
+	 * @param pathIndex
+	 * @param time
+	 * @return the specific realization of the Lévy process.
+	 */
 	double getSpecificRealizationAtGivenTime(int pathIndex, double time);
 
+	/**
+	 * This method prints the specific path of the Lévy process.
+	 * 
+	 * @param pathIndex
+	 */
 	void printSpecificPath(int pathIndex);
 
 }
